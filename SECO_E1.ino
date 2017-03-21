528c6f25008887034f34e17728a5a1c9521c0336
David González <dgfiloso95@gmail.com>
2017-03-21 16:30:21 +0100

    Consiguido PWM analógico

--- SECO_E1.ino
+++ SECO_E1.ino
@@ -1,77 +1,135 @@
 #include <DueTimer.h>
 
-uint32_t clka = 0;    //  No se usa
-uint32_t clkb = 0;    //  No se usa
-uint32_t mck = 0;     //  Master clock sin prescalado
-uint32_t pwmPin = 8; // PWM output pin
-uint32_t ul_channel = g_APinDescription[pwmPin].ulPWMChannel;   // Set PWM channel
-//  Explicado en la página 1044 del datasheet
-uint32_t prescaler = clka;
-uint32_t alignment = 0;   //  0 izquierda, 1 centro
-uint32_t polarity = 0;    //  0 comienza en nivel bajo, 1 comienza en nivel alto
-uint32_t period = 100;      //  Frecuencia 20kHz -> Periodo 50 us -> Usando MCK este campo es: T*MCK
-uint16_t duty = 50;
-uint16_t timeH = 0;
-uint16_t timeL = 0;
+uint32_t CPRD = 50*84;     //  Frecuencia 20kHz -> Periodo 50 us -> Usando MCK este campo es: T*MCK = 50 us * 84 MHz
 
+uint32_t channel_0 = 0;   // PWM channel 0
+uint16_t duty_cycle_0 = 0;       //  De 0 a CPRD
+uint32_t pwmPin35 = 35; // PWM output pin
+
+uint32_t channel_1 = 1;   // PWM Channel 1
+uint16_t duty_cycle_1 = 0;       //  De 0 a CPRD
+uint32_t pwmPin37 = 37; // PWM output pin
+  
 uint32_t dec1Pin = 5; // Decoder input pin 1
 uint32_t dec2Pin = 6; // Decoder input pin 2
 
-void setup() {
+//  Tal y como esta hecho, solo se pueden usar pines del periférico B
+void setupPWM(uint32_t channel, uint16_t duty, uint32_t pwmPin){
+  
+  uint32_t clka = 0;    //  No se usa
+  uint32_t clkb = 0;    //  No se usa
+  uint32_t mck = 0;     //  Master clock sin prescalado
+  //  Explicado en la página 1044 del datasheet
+  uint32_t prescaler = 0;
+  uint32_t alignment = 0;   //  0 izquierda, 1 centro
+  uint32_t polarity = 0;    //  0 comienza en nivel bajo, 1 comienza en nivel alto
 
-  //  Configuramos el puerto del PWM
-  PIO_Configure(
-   g_APinDescription[pwmPin].pPort,
-   g_APinDescription[pwmPin].ulPinType,
-   g_APinDescription[pwmPin].ulPin,
-   g_APinDescription[pwmPin].ulPinConfiguration);
-   
-  //  Configuramos el PWM
+  uint16_t timeH = 0;
+  uint16_t timeL = 0;
+
+  //  Habilitamos el periférico de PWM
   pmc_enable_periph_clk(ID_PWM);    //  ID del PWM, se encuentra en ../system/CMSIS/Device/ATMEL/Include/sam3x8e.h
 
   //  La constante PWM es la dirección base del PWM que se encuentra definida en ../system/CMSIS/Device/ATMEL/Include/sam3x8e.h
   PWMC_ConfigureClocks(clka, clkb, mck ) ;
-  PWMC_ConfigureChannel(  PWM,  ul_channel,  prescaler,  alignment,  polarity ) ;
-  PWMC_SetPeriod(  PWM,  ul_channel, period ) ; 
-  PWMC_SetDutyCycle(  PWM,  ul_channel,  duty ) ;
-  PWMC_SetDeadTime(  PWM,  ul_channel,  timeH,  timeL ) ;
+  PWMC_ConfigureChannel(  PWM,  channel,  prescaler,  alignment,  polarity ) ;
+  PWMC_SetPeriod(  PWM,  channel, CPRD ) ; 
+  PWMC_SetDutyCycle(  PWM,  channel,  duty ) ;
+  PWMC_SetDeadTime(  PWM,  channel,  timeH,  timeL ) ;
 
-  PWMC_EnableChannel(  PWM,  ul_channel ) ;
+  PWMC_EnableChannel(  PWM,  channel ) ;
 
-  //  Configuramos los pines de entrada del decoder
+  //  Configuramos el puerto del PWM
   PIO_Configure(
-    g_APinDescription[dec1Pin].pPort,
-    g_APinDescription[dec1Pin].ulPinType,
-    g_APinDescription[dec1Pin].ulPin,
-    g_APinDescription[dec1Pin].ulPinConfiguration);
-   
-  PIO_Configure(
-    g_APinDescription[dec2Pin].pPort,
-    g_APinDescription[dec2Pin].ulPinType,
-    g_APinDescription[dec2Pin].ulPin,
-    g_APinDescription[dec2Pin].ulPinConfiguration);
-
-  PIO_SetInput(
-    g_APinDescription[dec1Pin].pPort,
-    g_APinDescription[dec1Pin].ulPin,
-    g_APinDescription[dec1Pin].ulPinConfiguration);
-
-  PIO_SetInput(
-    g_APinDescription[dec1Pin].pPort,
-    g_APinDescription[dec1Pin].ulPin,
-    g_APinDescription[dec1Pin].ulPinConfiguration);
-    
-  Timer3.attachInterrupt(readDecoder).setPeriod(1000);    //  Cada 1000 us se lanza la función readDecoder()
-
+    g_APinDescription[pwmPin].pPort,
+    PIO_PERIPH_B,
+    g_APinDescription[pwmPin].ulPin,
+    g_APinDescription[pwmPin].ulPinConfiguration);
 }
 
-void loop() {
-  
-  Timer3.start();
-  
+int setVoltage(double v, uint32_t channel, uint32_t pwmPin) {
+  if (v >= 0 && v <= 9) {
+    uint16_t duty;
+    duty = (uint16_t)((v * (double)CPRD)/9);
+    setupPWM(channel,duty,pwmPin);
+    return 0;
+  } else {
+    return 1;
+  }
 }
 
 void readDecoder () {
   
 }
+
+void setup() {
+
+  Serial.begin(9600);
+    
+//  Timer3.attachInterrupt(readDecoder).setPeriod(1000);    //  Cada 1000 us se lanza la función readDecoder()
+//   Timer3.start();
+}
+
+void loop() {
+
+//  if ( setVoltage(0,channel_0,pwmPin35) ) {
+//    Serial.println("#Error: Voltaje fuera de rango [0,9]");
+//  }
+//  if ( setVoltage(4,channel_1,pwmPin37) ) {
+//    Serial.println("#Error: Voltaje fuera de rango [0,9]");
+//  }
+//
+//  delay(5000);
+//
+//  if ( setVoltage(4,channel_0,pwmPin35) ) {
+//    Serial.println("#Error: Voltaje fuera de rango [0,9]");
+//  }
+//  if ( setVoltage(0,channel_1,pwmPin37) ) {
+//    Serial.println("#Error: Voltaje fuera de rango [0,9]");
+//  }
+//
+//  delay(5000);
+
+  if ( setVoltage(0,channel_0,pwmPin35) ) {
+    Serial.println("#Error: Voltaje fuera de rango [0,9]");
+  }
+  if ( setVoltage(0,channel_1,pwmPin37) ) {
+    Serial.println("#Error: Voltaje fuera de rango [0,9]");
+  }
+
+  delay(5000);
+//  double tension;
+//  String sentido;
+//  
+//  Serial.println("Indique la tension");
+//  while(!Serial.available()) {
+//    tension = Serial.read();
+//  }
+//  Serial.println("Indique el sentido");
+//  while(!Serial.available()) {
+//    sentido = Serial.readString();
+//  }
+//  
+//  if ( sentido == "derecha") {
+//    if ( setVoltage(0,channel_0,pwmPin35) ) {
+//      Serial.println("#Error: Voltaje fuera de rango [0,9]");
+//    }
+//    if ( setVoltage(tension,channel_1,pwmPin37) ) {
+//      Serial.println("#Error: Voltaje fuera de rango [0,9]");
+//    }
+//  } else if ( sentido == "izquierda" ) {
+//    if ( setVoltage(tension,channel_0,pwmPin35) ) {
+//      Serial.println("#Error: Voltaje fuera de rango [0,9]");
+//    }
+//    if ( setVoltage(0,channel_1,pwmPin37) ) {
+//      Serial.println("#Error: Voltaje fuera de rango [0,9]");
+//    }
+//  } else {
+//    Serial.println("#Error: Sentido no definido, solo derecha o izquierda");
+//  }
+//
+//  tension = 0;
+//  sentido = "";
+}
+
 
